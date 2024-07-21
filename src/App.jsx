2424b80b2e8b83110231073abe63b2cb0766b695
Tran Hau <hautran.01022002@gmail.com>
2024-07-22 04:04:22 +0700

    Dark Light Mode P1

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,27 @@ import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
 
   return (
     <>
+      <ModeToggle></ModeToggle>
+      <hr />
       <div>TranVanHau</div>
       <Typography variant='body2' color="text.secondary">Test Typography</Typography>
       <Button variant="text">Text</Button>
